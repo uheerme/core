3b5d07868bf2e3f4431a3589330a1f9e7c8a3bca
Lucas David <lucas.david@drexel.edu>
2015-03-11 19:41:33 -0300

    Add IDateTrackable

---
+++ Samesound.Data/Interfaces/IDateTrackable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Samesound.Data.Interfaces
+{
+    public interface IDateTrackable
+    {
+        DateTime DateCreated { get; set; }
+        DateTime? DateUpdated { get; set; }
+    }
+}
