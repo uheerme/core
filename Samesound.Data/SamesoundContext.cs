2052fccddfdde8f366a092994a8cf43284a1668f
Lucas David <lucas.david@drexel.edu>
2015-03-11 19:09:23 -0300

    Add Samesound.Data

---
+++ Samesound.Data/SamesoundContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Samesound.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samesound.Data
+{
+    public class SamesoundContext : IdentityDbContext<ApplicationUser>
+    {
+        public SamesoundContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static SamesoundContext Create()
+        {
+            return new SamesoundContext();
+        }
+    }
+}
