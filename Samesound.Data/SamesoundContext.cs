8d2979abf1cac5a57dba9c0160ab759b9aab466b
Lucas David <lucas.david@drexel.edu>
2015-03-11 19:41:24 -0300

    Add and resolve dependencies for Service

--- Samesound.Data/SamesoundContext.cs
+++ Samesound.Data/SamesoundContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
 using Samesound.Core.Models;
+using Samesound.Data.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Samesound.Data
@@ -13,6 +16,39 @@ namespace Samesound.Data
         public SamesoundContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
+        }
+
+        public void TrackDate()
+        {
+            foreach (var entity in ChangeTracker.Entries().Where(p => p.State == EntityState.Added || p.State == EntityState.Modified))
+            {
+                if (entity.State == EntityState.Added && entity.Entity is IDateTrackable)
+                {
+                    ((IDateTrackable)entity.Entity).DateCreated = DateTime.Now;
+                }
+                if (entity.State == EntityState.Modified && entity.Entity is IDateTrackable)
+                {
+                    ((IDateTrackable)entity.Entity).DateUpdated = DateTime.Now;
+                }
+            }
+        }
+
+        public override int SaveChanges()
+        {
+            TrackDate();
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync()
+        {
+            TrackDate();
+            return base.SaveChangesAsync();
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            TrackDate();
+            return base.SaveChangesAsync(cancellationToken);
         }
 
         public static SamesoundContext Create()
