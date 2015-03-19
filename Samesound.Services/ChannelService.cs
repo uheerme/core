ac88584b52db9225a01e56f931918fd24f4771cf
Lucas David <lucas.david@drexel.edu>
2015-03-18 23:16:54 -0300

    Fix #2

---
+++ Samesound.Services/ChannelService.cs
@@ -0,0 +1,37 @@
+﻿using Samesound.Core;
+using Samesound.Data;
+using Samesound.Services.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Samesound.Services
+{
+    public class ChannelService : Service<Channel>
+    {
+        public ChannelService(SamesoundContext db) : base(db) { }
+
+        public virtual async Task<ICollection<Channel>> Paginate(int skip, int take)
+        {
+            return await Db.Channels
+                .OrderBy(c => c.Id)
+                .Skip(skip)
+                .Take(take)
+                .ToListAsync();
+        }
+
+        public virtual async Task<int> Play(Channel channel, Music music)
+        {
+            if (!channel.Musics.Any(m => m.Id == music.Id))
+            {
+                throw new ApplicationException();
+            }
+
+            channel.Playing = music;
+
+            return await Update(channel);
+        }
+    }
+}
