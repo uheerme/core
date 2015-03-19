51437dd461be060cdb2c86ee21a205b8a7ddc317
Lucas David <lucas.david@drexel.edu>
2015-03-18 23:20:05 -0300

    Update Scripts

---
+++ Samesound/Scripts/app/app.js
@@ -0,0 +1,13 @@
+﻿// Samesound Angular Application.
+// Defines the main module of the application.
+
+'use strict';
+
+var samesoundApp = angular
+    .module('samesoundApp', [
+        'ngRoute',
+        'ngAnimate'
+    ])
+    .constant('config', {
+        url: 'http://localhost:1330/'
+    });
