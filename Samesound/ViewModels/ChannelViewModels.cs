447397b0641d36d8d3a4fa70665140556611c9cb
Lucas David <lucas.david@drexel.edu>
2015-03-18 23:19:40 -0300

    Update ViewModels

---
+++ Samesound/ViewModels/ChannelViewModels.cs
@@ -0,0 +1,78 @@
+﻿using Samesound.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Samesound.ViewModels
+{
+    public class ChannelCreateViewModel
+    {
+        [Required]
+        [StringLength(128, MinimumLength=4)]
+        public string Name { get; set; }
+        
+        [Required]
+        public string Owner { get; set; }
+
+        [Required]
+        [StringLength(256, MinimumLength=32)]
+        public string NetworkIdentifier { get; set; }
+
+        public static explicit operator Channel(ChannelCreateViewModel m)
+        {
+            return new Channel
+            {
+                Name  = m.Name,
+                Owner = m.Owner,
+                NetworkIdentifier = m.NetworkIdentifier
+            };
+        }
+    }
+
+    public class ChannelUpdateViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public void Update(Channel c)
+        {
+            c.Name = Name;
+        }
+    }
+
+    public class ChannelResultViewModel
+    {
+        public int       Id    { get; set; }
+        public string    Name  { get; set; }
+        public string    Owner { get; set; }
+        public string    NetworkIdentifier { get; set; }
+        public ICollection<MusicResultViewModel> Musics { get; set; }
+        public MusicResultViewModel Playing { get; set; }
+        public DateTime  DateCreated  { get; set; }
+        public DateTime? DateUpdated { get; set; }
+
+        public static explicit operator ChannelResultViewModel(Channel c)
+        {
+            return new ChannelResultViewModel
+            {
+                Id                = c.Id,
+                Name              = c.Name,
+                Owner             = c.Owner,
+                NetworkIdentifier = c.NetworkIdentifier,
+                DateCreated       = c.DateCreated,
+                DateUpdated       = c.DateUpdated,
+                
+                Musics = c.Musics
+                    .Select(m => (MusicResultViewModel)m)
+                    .ToList(),
+                Playing = (MusicResultViewModel)c.Playing
+                
+            };
+        }
+    }
+}
---
+++ Samesound/ViewModels/MusicViewModels.cs
@@ -0,0 +1,93 @@
+﻿using Samesound.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Samesound.ViewModels
+{
+    public class MusicCreateViewModel
+    {
+        [Required]
+        [StringLength(128, MinimumLength=4)]
+        public string Name     { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int SizeInBytes { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int LengthInMinutes { get; set; }
+
+        [Required]
+        public int ChannelId { get; set; }
+        
+        public static explicit operator Music(MusicCreateViewModel m)
+        {
+            return new Music
+            {
+                Name            = m.Name,
+                SizeInBytes     = m.SizeInBytes,
+                LengthInMinutes = m.LengthInMinutes,
+                ChannelId       = m.ChannelId
+            };
+        }
+    }
+
+    public class MusicUpdateViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(128, MinimumLength = 4)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int SizeInBytes { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int LengthInMinutes { get; set; }
+
+        [Required]
+        public int ChannelId { get; set; }
+        
+        public void Update(Music c)
+        {
+            c.Name            = Name;
+            c.SizeInBytes     = SizeInBytes;
+            c.LengthInMinutes = LengthInMinutes;
+            c.ChannelId       = ChannelId;
+        }
+    }
+
+    public class MusicResultViewModel
+    {
+        public int    Id   { get; set; }
+        public string Name { get; set; }
+        public int SizeInBytes     { get; set; }
+        public int LengthInMinutes { get; set; }
+        public int ChannelId       { get; set; }
+        public DateTime  DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
+
+        public static explicit operator MusicResultViewModel(Music c)
+        {
+            return new MusicResultViewModel
+            {
+                Id                = c.Id,
+                Name              = c.Name,
+                SizeInBytes       = c.SizeInBytes,
+                LengthInMinutes   = c.LengthInMinutes,
+                ChannelId         = c.ChannelId,
+                DateCreated       = c.DateCreated,
+                DateUpdated       = c.DateUpdated
+            };
+        }
+    }
+}
