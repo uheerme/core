09e7048803df43c3f5f210e79f0989f8bc22a6bd
Lucas David <lucas.david@drexel.edu>
2015-03-18 23:18:25 -0300

    Fix #5

---
+++ Samesound.Services/MusicService.cs
@@ -0,0 +1,24 @@
+﻿using Samesound.Core;
+using Samesound.Data;
+using Samesound.Services.Infrastructure;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Samesound.Services
+{
+    public class MusicService : Service<Music>
+    {
+        public MusicService(SamesoundContext db) : base(db) { }
+
+        public virtual async Task<ICollection<Music>> Paginate(int skip, int take)
+        {
+            return await Db.Musics
+                .OrderBy(m => m.Id)
+                .Skip(skip)
+                .Take(take)
+                .ToListAsync();
+        }
+    }
+}
