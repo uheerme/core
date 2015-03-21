9f9ac5a4e273f589a412fff933aacdef50416af9
Lucas David <lucas.david@drexel.edu>
2015-03-21 18:21:09 -0300

    Add DetailedErrorPolicy in global configuration

--- Samesound/Global.asax.cs
+++ Samesound/Global.asax.cs
@@ -18,6 +18,8 @@ namespace Samesound
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            GlobalConfiguration.Configuration.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
         }
     }
 }
