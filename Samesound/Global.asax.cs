32c0dcb20dd8b483423648ae51735a58a96b1d21
Lucas David <lucas.david@drexel.edu>
2015-03-21 18:34:57 -0300

    Add Database initializer to Global

--- Samesound/Global.asax.cs
+++ Samesound/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Samesound.Data;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -13,13 +15,15 @@ namespace Samesound
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configuration.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            GlobalConfiguration.Configuration.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
+            Database.SetInitializer<SamesoundContext>(null);
         }
     }
 }
