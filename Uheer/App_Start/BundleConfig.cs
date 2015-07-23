04b7150764d5d4cabaedef29e20724afea1dc4e8
Lucas David <lucas.david@drexel.edu>
2015-07-23 10:47:21 -0300

    Add navbar

--- Uheer/App_Start/BundleConfig.cs
+++ Uheer/App_Start/BundleConfig.cs
@@ -18,8 +18,6 @@ namespace Uheer
                 "~/Scripts/jquery-{version}.js",
                 "~/Scripts/toastr.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/code/modernizr").Include(
                 "~/Scripts/modernizr-*"));
 
@@ -41,6 +39,7 @@ namespace Uheer
                 "~/Scripts/ng-file-upload-all.min.js",
                 "~/Scripts/app/app.js",
                 "~/Scripts/app/services/*.js",
+                "~/Scripts/app/modules/shared/*.js",
                 "~/Scripts/app/modules/home/*.js",
                 "~/Scripts/app/modules/account/*.js",
                 "~/Scripts/app/modules/channels/*.js",
---
+++ Uheer/Scripts/app/modules/shared/NavbarController.js
@@ -0,0 +1,7 @@
+﻿'use strict';
+
+function NavbarController($scope, Authority) {
+    $scope.a = Authority.authentication;
+}
+
+UheerApp.controller('NavbarController', ['$scope', 'Authority', NavbarController]);
